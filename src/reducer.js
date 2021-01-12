8ec28e463dbcd51a5b2d6014f8a22324dbec4c6e
Buntybru <buntybru@gmail.com>
2021-01-12 21:16:56 +0530

    added the checkout page functionalities, adding element , removing element etc

--- src/reducer.js
+++ src/reducer.js
@@ -2,16 +2,32 @@ export const initialState = {
   basket: []
 };
 
+//Selector
+export const getBasketTotal = basket =>
+  basket.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
-  console.log("diapatch", action);
+  console.log("dispatch", action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       return { ...state, basket: [...state.basket, action.item] };
-      break;
+
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        basketItem => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn("cannot remove");
+      }
+
+      return { ...state, basket: newBasket };
 
     default:
       return state;
-      break;
   }
 };
 
